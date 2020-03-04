21a603cf5215e5735c9c0dbe909b65aa07fc50ee
haroldo <haroldo.gmail.com>
2020-03-03 23:51:10 -0300

    implementação de Krooze.EntranceTest.WriteHere.Tests.InjectionTests.GetCruises

--- Krooze.EntranceTest.WriteHere/Tests/InjectionTests/InjectionTest.cs
+++ Krooze.EntranceTest.WriteHere/Tests/InjectionTests/InjectionTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Krooze.EntranceTest.WriteHere.Structure.Model;
 
 namespace Krooze.EntranceTest.WriteHere.Tests.InjectionTests
@@ -11,6 +13,38 @@ namespace Krooze.EntranceTest.WriteHere.Tests.InjectionTests
             //There is an interface (IGetCruise) that is implemented by 3 classes (Company1, Company2 and Company3)
             //Make sure that the correct class is injected based on the CruiseCompanyCode on the request
             //without directly referencing the 3 classes and the method GetCruises of the chosen implementation is called
+            //return null;
+
+            Type t = null;
+
+            if (request.CruiseCompanyCode == 1)
+            {
+                t = Type.GetType("Krooze.EntranceTest.WriteHere.Structure.Implementations.Company1");
+                ConstructorInfo ctor = t.GetConstructor(Type.EmptyTypes);
+                var obj = ctor.Invoke(null);
+                MethodInfo mi = t.GetMethod("GetCruises");
+                var list = mi.Invoke(obj, new Object[] { request });
+                return (List<CruiseDTO>)list;
+            }
+            else if (request.CruiseCompanyCode == 2)
+            {
+                t = Type.GetType("Krooze.EntranceTest.WriteHere.Structure.Implementations.Company2");
+                ConstructorInfo ctor = t.GetConstructor(Type.EmptyTypes);
+                var obj = ctor.Invoke(null);
+                MethodInfo mi = t.GetMethod("GetCruises");
+                var list = mi.Invoke(obj, new Object[] { request });
+                return (List<CruiseDTO>)list;
+            }
+            else if (request.CruiseCompanyCode == 3)
+            {
+                t = Type.GetType("Krooze.EntranceTest.WriteHere.Structure.Implementations.Company3");
+                ConstructorInfo ctor = t.GetConstructor(Type.EmptyTypes);
+                var obj = ctor.Invoke(null);
+                MethodInfo mi = t.GetMethod("GetCruises");
+                var list = mi.Invoke(obj, new Object[] { request });
+                return (List<CruiseDTO>)list;
+            }
+
             return null;
         }
     }
